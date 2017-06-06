0cc7249d1b034b61f23e19e979bba9a6f9754734
Jordan Stubblefield <jstubblefield7939@gmail.com>
2017-06-06 16:28:53 -0500

    refactor debounce to not suck

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,33 +1,36 @@
 class Search extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      value: ''
+      searchQuery: ''
     };
-    this.handleSearch = _.debounce(this.handleInputChange, 1000);
+    this.debounceQuery = _.debounce(this.handleQuery, 1000);
   }
 
-  handleInputChange(value) {
-    this.props.handleSearchInputChange(value);
+  handleQuery(e) {
+    this.props.getYoutubeVideos(e);
+
     this.setState({
-      value: value
+      searchQuery: e
     });
   }
 
   render() {
     return (
-      <div className='search-bar form-inline'>
-        <input 
-          className='form-control' type='text' 
-          value={this.state.value} 
-          onChange={e => this.handleSearch(e.target.value)}/>
-        <button className='btn hidden-sm-down'>
-          <span className='glyphicon glyphicon-search'></span>
+      <div className="search-bar form-inline">
+        <input
+          className="form-control"
+          type="text"
+          onChange={e => this.debounceQuery(e.target.value)}
+        />
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
         </button>
-      </div> 
+      </div>
     );
   }
 }
 
+// In the ES6 spec, files are "modules" and do not share a top-level scope
+// `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
